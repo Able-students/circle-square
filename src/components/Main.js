d00e22e7867d33f8c19468567e01bb56a8ec7aff
Arman-Zh <lunatik84@bk.ru>
2023-03-23 20:45:15 +0600

    radio box

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,8 @@
 import { useEffect, useMemo } from "react";
 import mainActions from "../store/actions/mainActions";
 import { useDispatch, useSelector } from "react-redux";
-import { Checkbox, Drawer, Button } from 'antd';
+import { Checkbox, Drawer, Button, Radio, Space } from 'antd';
+
 import { useState } from "react";
 import { MenuOutlined } from '@ant-design/icons';
 
@@ -15,66 +16,84 @@ const Main = () => {
     const [colors, setColors] = useState([]);
     const [colorsCheckbox, setColorsCheckbox] = useState(colors);
     const [colorsUpdate, setColorsUpdate] = useState(false);
+    const [value, setValue] = useState("all");
+    const filteredList = useMemo(() => filterArray(list), [list, circleChecked, squareChecked, trgleChecked, value, colorsCheckbox.length]);
 
-    const filteredList = useMemo(() => filterArray(list),[list,circleChecked,squareChecked,colorsCheckbox.length])
- 
     useEffect(() => {
         dispatch(mainActions.loadJsonList())
-    },[])
+    }, [])
 
     useEffect(() => {
         let colors = []
         list?.forEach(elem => {
-            if(!colors.includes(elem.color) && elem.color !== 'greed'){
+            if (!colors.includes(elem.color) && elem.color !== 'greed') {
                 colors.push(elem.color)
             }
         })
         setColors(colors)
         setColorsCheckbox(colors)
-    },[list])
+    }, [list])
 
     const showDrawer = () => {
         setOpen(true);
-      };
-    
+    };
+
     const onClose = () => {
         setOpen(false);
     };
 
     const onChange = (e) => {
-        if(e.target.name === 'circle'){
+        if (e.target.name === 'circle') {
             setCircleChecked(e.target.checked)
-        }else if(e.target.name === 'square'){
+        } else if (e.target.name === 'square') {
             setSquareChecked(e.target.checked)
-        }else{
+        } else if (e.target.name === 'triangle') {
             settrgleChecked(e.target.checked)
         }
     }
 
+    const onChange1 = (e) => {
+        setValue(e.target.value);
+    };
+
     const onColorChange = (e) => {
         let newList = colorsCheckbox
-        if(e.target.checked){
+        if (e.target.checked) {
             newList.push(e.target.name)
-        }else{
+        } else {
             newList = newList.filter(elem => elem !== e.target.name)
         }
         setColorsCheckbox(newList)
         setColorsUpdate(!colorsUpdate)
     }
 
-    function filterArray(list){
+    function filterArray(list) {
         let newList1 = list.filter(elem => circleChecked && (elem.form === 'circle' || elem.form === 'circa'))
         let newList2 = list.filter(elem => squareChecked && elem.form === 'square')
         let newList3 = list.filter(elem => trgleChecked && elem.form === 'triangle')
         let filteredArr = newList1.concat(newList2).concat(newList3)
-        return filteredArr.filter(elem => colorsCheckbox.some(item => item === elem.color))
+        let colorFilter = filteredArr.filter(elem => colorsCheckbox.some(item => item === elem.color))
+
+        let arr = colorFilter.filter(el => {
+            if (value === "dark" && el.dark) {
+                return true
+            } else if (value === "light" && !el.dark) {
+                return true
+            } else if (value === "all") {
+                return true
+            } else {
+                return false
+            }
+        })
+
+        return arr
     }
 
-    return(
+    return (
         <div>
             <h1>Круги и квадраты, v.1.0</h1>
             <div className="row">
-                <Button type="primary" onClick={showDrawer} style={{marginRight: '20px'}}>
+                <Button type="primary" onClick={showDrawer} style={{ marginRight: '20px' }}>
                     <MenuOutlined />
                 </Button>
                 <Checkbox onChange={onChange} checked={circleChecked} name="circle">Circle</Checkbox>
@@ -83,18 +102,18 @@ const Main = () => {
             </div>
 
             <div className="grid">
-            {
-                filteredList.map((item,i) => (
-                    <div key={i}
-                        className={'div-' + item.form} 
-                        style={{
-                                backgroundColor: item.form !== 'triangle' ? item.color : 'transparent', 
-                                filter: `brightness(${item.dark ? 1.5 : 0.5})`,
+                {
+                    filteredList.map((item, i) => (
+                        <div key={i}
+                            className={'div-' + item.form}
+                            style={{
+                                backgroundColor: item.form !== 'triangle' ? item.color : 'transparent',
+                                filter: `brightness(${item.dark ? 0.5 : 1.5})`,
                                 borderBottomColor: item.color,
-                                }}>
-                    </div>
-                ))
-            }
+                            }}>
+                        </div>
+                    ))
+                }
             </div>
             <Drawer
                 title="Filters"
@@ -107,12 +126,19 @@ const Main = () => {
                     {colors?.map(item => (
                         <div key={item} className="container">
                             <label className="container">
-                                <input type="checkbox" checked={colorsCheckbox?.includes(item)} onChange={onColorChange} name={item}/>
-                                <span className="checkmark" style={{backgroundColor: item}}></span>
-                                {item}  
+                                <input type="checkbox" checked={colorsCheckbox?.includes(item)} onChange={onColorChange} name={item} />
+                                <span className="checkmark" style={{ backgroundColor: item }}></span>
+                                {item}
                             </label>
                         </div>
                     ))}
+                    <Radio.Group onChange={onChange1} value={value}>
+                        <Space direction="vertical">
+                            <Radio value="all">Все</Radio>
+                            <Radio value="dark">Темные</Radio>
+                            <Radio value="light">Светлые</Radio>
+                        </Space>
+                    </Radio.Group>
                 </div>
             </Drawer>
         </div>
